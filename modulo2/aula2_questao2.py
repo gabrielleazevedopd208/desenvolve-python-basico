0b40c326733071c74ca2aecd9a763226b5d5292f
elle <gabrielleazevedo.pdbd208@pdbomdespacho.com.br>
2025-09-29 10:46:30 -0300

    Add files via upload

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,13 @@
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra variável.
+#Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+r = "o resultado é"
+v1 = 10
+v2 = 3.5
+#soma
+s = v1 + v2
+print(type(r))
+print(type(v1))
+print(type(v2))
+print(type(s))
